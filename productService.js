46bdd1fef6871c3b60dcd03532e8217973f5e617
Viviane Araujo <developer.viviane@gmail.com>
2023-06-15 16:05:45 -0300

    Finalizado método getProduct

--- productService.js
+++ productService.js
@@ -1,7 +1,11 @@
 async function getProduct(productId) {
     const apiUrl = `https://fakestoreapi.com/products/${productId}`;
 
-    // Add your solution here!
+    fetch(apiUrl)
+        .then(response => response.json())        
+        .catch(err => console.error("Tivemos um erro na chamada API", err));
 }
 
 module.exports = getProduct;
+
+
