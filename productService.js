b6cf98427bab024274497f7aa770646110240644
Viviane Araujo <developer.viviane@gmail.com>
2023-06-15 16:28:42 -0300

    Correcao do método productService

--- productService.js
+++ productService.js
@@ -2,10 +2,16 @@ async function getProduct(productId) {
     const apiUrl = `https://fakestoreapi.com/products/${productId}`;
 
     fetch(apiUrl)
-        .then(response => response.json())        
+        .then(response => response.json())  
+        .then((data) => {
+            console.log(data);
+            return data;
+          })                         
         .catch(err => console.error("Tivemos um erro na chamada API", err));
+        
 }
 
 module.exports = getProduct;
 
+console.log(getProduct(10))
 
